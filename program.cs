8cfd11bbaae52bb12c59e2128b3df9b6102c5e46
ma-riry <mariamariry@gmail.com>
2023-09-13 09:24:06 +0200

    modified - deleted unnecessary files

--- program.cs
+++ program.cs
@@ -1,114 +1,58 @@
-using System;
-
-namespace CashRegister
-{
-    class DeliveryService
-    {
-        static void Delivery(string[] args)
-        {
-           
-            Delivery personal = new PersonalPickup();
-            Delivery mail = new MailDelivery();
-            Delivery courier = new CourierService();
+string [] man = new string [] {"Wacom", "Huion", "XP-Pen", "Gaomon", "Apple"};
+string [] color = new string [] {"white", "black", "grey"};
 
 
+Object tablet = new Object(man[0], color[0], 2000, true); // in
+Object tablet2 = new Object(man[1], color[1], 3500, true);
+Object tablet3 = new Object(man[2], color[2], 5000, true);
+Object tablet4 = new Object(man[3], color[1], 2500, false);
+Object tablet5 = new Object(man[4], color[2], 3000, false);
 
+Console.WriteLine("***************************************************");
 
-           DisplayDelivery("STANDARD DELIVERY", personal);
-           DisplayDelivery("MAIL DELIVERY", mail);
-           DisplayDelivery("COURIER SERVICE", courier);
+Console.WriteLine("Manufacturer: {0}", tablet.man [0]); // out
+Console.WriteLine("Color: {0}", tablet.color [0]);
+Console.WriteLine("Resolution: {0}", tablet.ppi); 
+Console.WriteLine("Pen Pressure: {0}", tablet.pressure ? "yes" : "no"); 
 
+Console.WriteLine("***************************************************");
 
-        }
-        static void DisplayDelivery(string deliveryType, Delivery delivery)
-        {
-            Console.WriteLine($"=====================================");
-            Console.WriteLine(deliveryType);
-            Console.WriteLine($"-------------------------------------");
-            Console.WriteLine($"- Address: {delivery.Address}");
-            Console.WriteLine($"- City: {delivery.City}");
-            Console.WriteLine($"- Phone: {delivery.Phone}");
-            Console.WriteLine($"- DateTime: {delivery.DateTime}");
-        }
-    }
+Console.WriteLine("Manufacturer: {1}", tablet2.man[1]); 
+Console.WriteLine("Color: {1}", tablet2.color[1]);
+Console.WriteLine("Resolution: {0}", tablet2.ppi); 
+Console.WriteLine("Pen Pressure: {0}", tablet2.pressure ? "yes" : "no"); 
 
+Console.WriteLine("***************************************************");
 
-    interface Delivery
-    {
-        string Address { get; set; }
-        string City { get; set; }
-        string Phone { get; set; }
-        string DateTime { get; set; }
-    }
+Console.WriteLine("Manufacturer: {2}", tablet3.man[2]); 
+Console.WriteLine("Color: {2}", tablet3.color[2]);
+Console.WriteLine("Resolution: {0}", tablet3.ppi); 
+Console.WriteLine("Pen Pressure: {0}", tablet3.pressure ? "yes" : "no"); 
 
+Console.WriteLine("***************************************************");
 
+Console.WriteLine("Manufacturer: {3}", tablet4.man[3]); 
+Console.WriteLine("Color: {1}", tablet4.color[1]);
+Console.WriteLine("Resolution: {0}", tablet4.ppi); 
+Console.WriteLine("Pen Pressure: {0}", tablet4.pressure ? "yes" : "no"); 
 
+Console.WriteLine("***************************************************");
 
-    class PersonalPickup : Delivery
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Phone { get; set; }
-        public string DateTime { get; set; }
+Console.WriteLine("Manufacturer: {4}", tablet5.man[4]); 
+Console.WriteLine("Color: {2}", tablet5.color[2]);
+Console.WriteLine("Resolution: {0}", tablet5.ppi); 
+Console.WriteLine("Pen Pressure: {0}", tablet5.pressure ? "yes" : "no"); 
 
+class Object {
+    public string man;
+    public string color; 
+    public int ppi;
+    public bool pressure; // yes true, no false
 
-
-
-        public PersonalPickup()
-        {
-            FirstName = "Clara";
-            LastName = "Tyler";
-            Address = "Ulica Republike Hrvatske 12";
-            City = "Požega";
-            Phone = "034-272-199";
-            DateTime = "05/09/2023 16:08:24";
-        }
-    }
-
-
-
-
-    class MailDelivery : Delivery
-    {
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Phone { get; set; }
-        public string DateTime { get; set; }
-
-
-
-
-         public MailDelivery()
-        {
-           
-            Address = "Cvjetna 15";
-            City = "Požega";
-            Phone = "034-275-543";
-            DateTime = "05/09/2023 17:09:43";
-        }
-    }
-
-
-
-
-    class CourierService : Delivery
-    {
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Phone { get; set; }
-        public string DateTime { get; set; }
-
-
-
-
-         public CourierService()
-        {
-            Address = "Ul. Stjepana Radića 10";
-            City = "Požega";
-            Phone = "034-274-178";
-            DateTime = "05/09/2023 14:02:25";
-        }
+    public Object (string man, string color, int ppi, bool pressure) {
+        this.man = man;
+        this.color = color;
+        this.ppi = ppi;
+        this.pressure = pressure;
     }
 }
